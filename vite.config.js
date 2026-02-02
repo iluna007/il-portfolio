26cfb5d18d1822a0d447ffdafc4530194258aa6c
Iker Luna <146373472+iluna007@users.noreply.github.com>
2026-02-01 22:30:12 -0600

    cambios para vite

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/il-portfolio/',
+  // ⚠️ Netlify sirve desde /
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
